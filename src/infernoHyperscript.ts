58350cec221d868f79e9fc8ec989c0cf3b12b479
Leonardo Saracini <leonardo.saracini@algoritmica.net>
2018-01-22 21:25:54 +0100

    add comment

--- src/infernoHyperscript.ts
+++ src/infernoHyperscript.ts
@@ -1,3 +1,6 @@
+/**
+* Useful to export html object an h function to easy create html node
+*/
 import h from 'inferno-hyperscript';
 import hh from 'hyperscript-helpers';
 export { default as h } from 'inferno-hyperscript';
