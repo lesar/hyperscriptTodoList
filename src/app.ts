0c639e25621fdf0a8435f04d2c7ea00bb4652e0f
Leonardo Saracini <leonardo.saracini@algoritmica.net>
2018-01-22 13:14:15 +0100

    added some comment

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,54 @@
+/**
+* App component is defined here together some basic interface.
+* App component is a container for all application components.
+*/
 import Footer from './footer';
 import { AddTodo } from './todos/todo/addTodo';
 import  VisibleTodoList from './todos/visibleToDoList';
 import Component from 'inferno-component';
 import { html, h } from './infernoHyperscript';
 import { ITodo } from './todos/todo/todo';
-
+/**
+* enum play filter rule on Completed todos property
+*/
 export enum CompletedFilter {
 	All=1,
 	Completed,
 	Uncompleted,
 }
+/**
+* enum play filter rule on Enabled todos property
+*/
 export enum EnabledFilter {
 	All=1,
 	Enabled,
 	Disabled,
 }
+/**
+* Filter interface containig all used filter
+*/
 export interface IFilter {
 	completed: CompletedFilter;
 	enabled: EnabledFilter;
 }
+/**
+* State interface. State is the unique global object containig the application state
+*/
 export interface IState {
 	todos: ITodo[];
 	visibilityFilter: IFilter;
 }
-export interface IStoreParam {
-	store: any;
-}
 
+/**
+* Initial filter object: all are showed
+*/
 export var filterAll: IFilter={
 	completed: CompletedFilter.All,
 	enabled: EnabledFilter.All
 };
+/**
+* all component node have to be instantiated by hyperscript h function
+*/
 export class App extends Component<any, IState> {
 	constructor(props: any) {
     super(props);
