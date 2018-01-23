ae71c0eb4e31a944c49ea9fda27a2c36f073ac49
Leonardo Saracini <leonardo.saracini@algoritmica.net>
2018-01-23 09:58:13 +0100

    comment added

--- src/filterLinks.ts
+++ src/filterLinks.ts
@@ -4,28 +4,26 @@ import { Link } from './link';
 import { IState, IFilter } from './app';
 
 /**
- * mapStateToProps: questa funzione permette di aggiornare automaticamente
- * un componente una volta cambiato lo store: appena lo store cambia chiama
- * questa funzione passandogli lo stato. tramite lo stato viene calcolato un
- * nuovo oggeto di proprietà che viene fuso con le proprietà del componente
- * ownProps: proprietà attuali del componente
- * state: stato attuale
- *
- * rende un oggetto piatto che verrà fuso tramite merge con le
- * proprietà del componente. questo oggetto viene spesso chiamato selector
- * e viene spesso usata reselect per integrare questo dato:
- * https://github.com/reactjs/reselect
+ * mapStateToProps: this function allows you to update automatically
+ * a component once changed the store: as soon as the store changes it calls
+ * this function passing his status, optional is passed ownProps: current
+ * properties of the component. A new property object that will be merged with
+ * the properties of the component is returned. this object is often called
+ * a selector and [reselect](https://github.com/reactjs/reselect)
+ * is often used to integrate this data:
  */
 const mapStateToProps = (state: IState) => ({
   filter: state.visibilityFilter
 });
 
 /**
- * se è un oggetto: ogni metodo è considerato un creatore di azioni redux
- * racchiuso in una chiamata dispach. L'oggetto verrà fuso con le proprietà
- * del componente.
- * se è una funzione: devi usare il dispach fornito e le proprietà per formare
- * un nuovo metodo che verrà fuso con le proprietà del componente
+ * If this is an object every property will be traslate in an
+ * action creator wrapped into a dispach call. This object wil be merged with
+ * the ownProps: the current properties of the component.
+ * If this is a function you have to use the provided dispatch and the optional
+ * ownProps (the current properties of the component) to return a new object that
+ * have every property as a method dispaccing a new action.
+ * This object will be merged with ownProps
  */
 const mapDispatchToProps = (dispatch: any) => ({
   onClick: (filter: IFilter) => {
@@ -34,7 +32,7 @@ const mapDispatchToProps = (dispatch: any) => ({
 });
 
 /**
- * FilterLink è un nuovo oggetto di tipo Link ma collegato ai cambiamenti dello store
+ * FilterLink is a new Link component but is connected to all store change
  */
 const FilterLink = connect(
   mapStateToProps,
