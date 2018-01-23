a45b40ebdf3d2d5913c5fed949ecbc0bb0a93a3b
Leonardo Saracini <leonardo.saracini@algoritmica.net>
2018-01-23 11:58:21 +0100

    remove typedoc and typedoc-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,20 +5,11 @@
 
 var webpack = require('webpack');
 const merge = require('webpack-merge');
-var TypedocWebpackPlugin = require('typedoc-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   plugins: [
-    new TypedocWebpackPlugin({
-      out: './docs',
-    	module: 'ES6',
-    	target: 'ES6',
-    	exclude: '**/node_modules/**/*.*',
-    	experimentalDecorators: true,
-    	excludeExternals: true
-    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
