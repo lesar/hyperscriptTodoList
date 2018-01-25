d38397d103cd800a890fa975534f53b224f51e70
Leonardo Saracini <leonardo.saracini@algoritmica.net>
2018-01-25 19:34:29 +0100

    add typedoc plugin configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,12 +7,25 @@ var webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
 
 module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
+    new TypedocWebpackPlugin({
+      out: './docs',
+    	module: 'commonjs',
+      mode: 'modules',
+    	target: 'es6',
+    	exclude: '**/node_modules/**/*.*',
+      ignoreCompilerErrors: true,
+      includeDeclarations: true,
+      name: 'hyperscriptTodoList',
+    	experimentalDecorators: true,
+    	excludeExternals: true
+    }, './src'),
     new UglifyJSPlugin({
       sourceMap: true,
       uglifyOptions: {
